369b4f58b8fb331e037cc9bbe5f7e80a158c6c7a
Thomas Klebel <t.klebel@hotmail.com>
2020-08-14 14:22:04 +0200

    try the same extraction with R

---
+++ extract_references.R
@@ -0,0 +1,17 @@
+library(tidyverse)
+library(stringr)
+
+readr::guess_encoding("data/rand_npl.csv")
+df <- read_csv("data/rand_npl.csv")
+
+df %>%
+  select(-rand, -country_code, -type) -> df_clean
+
+
+df_clean %>%
+  head(50) %>%
+  mutate(authors = str_extract(npl_text, ".*(?=:)"),
+         test = str_extract(npl_text, ".*:"))
+
+
+
